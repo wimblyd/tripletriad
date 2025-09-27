4d39422b1f47da6901fef3182b6a0ef0518ed2b6
wimblyd <151911549+wimblyd@users.noreply.github.com>
2025-09-27 11:47:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+// Back of the card
+const backImage = "img/TTBackLg.png";
+
+// Flip card front
+function flipCard(card, frontImage) {
+  card.src = frontImage;
+  saveCardState(card.dataset.cardId, frontImage);
+}
+
+// Flip card back
+function unflipCard(card) {
+  card.src = backImage;
+  saveCardState(card.dataset.cardId, backImage);
+}
+
+// Save state
+function saveCardState(id, src) {
+  localStorage.setItem(id, src);
+}
+
+// Assign IDs automatically + restore state
+window.onload = function() {
+  let counter = 1;
+
+  document.querySelectorAll("table img").forEach(card => {
+    // give each card a unique "card-#" ID if it doesn't have one
+    if (!card.dataset.cardId) {
+      card.dataset.cardId = "card-" + counter++;
+    }
+
+    // restore its saved state
+    const saved = localStorage.getItem(card.dataset.cardId);
+    if (saved) {
+      card.src = saved;
+    }
+  });
+};
+
 // Path to the back image
 const backImage = "img/TTBackLg.png";
 
