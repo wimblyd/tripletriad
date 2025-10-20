6af130ebf9583faac6536e03055c15c15a38af2f
Wimbly Donner <151911549+wimblyd@users.noreply.github.com>
2025-10-20 17:08:14 -0400

    Update win95.js

--- win95.js
+++ win95.js
@@ -99,6 +99,7 @@ document.addEventListener("DOMContentLoaded", () => {
     { icon: "img/icon-save.png", text: "Autosaves to localStorage" },
     { icon: "img/ChocoboWorld.png", text: "Launch Chocobo World" },
     { icon: "img/icon-keys.png", text: "Keyboard Controls for cw.exe »" },
+    { icon: "img/icon-phone.png", text: "Mobile Controls for cw.exe »" },
     { icon: "img/icon-screensaver.png", text: "Launch Screensaver" },
     { icon: "img/icon-thanks.png", text: "Special Thanks »" }
   ];
@@ -111,6 +112,7 @@ document.addEventListener("DOMContentLoaded", () => {
     "Open": ["Open with Button on the Start Bar", "Or Open from This Menu too"],
     "Restore": ["Click here to Restore", "Or Restore by Closing Manually"],
     "Keyboard": ["Use Arrow Keys to Move", "Space to Jump", "R & E Keys for Choco World Events", "Just for Funsies", "Not connectable to FFVIII"],
+    "Mobile": ["Swipe to Move", "Tap to Jump," "Double Tap for Choco World Events"]
     "Thanks": ["Triple Triad Backs: McIndusMods", "FF Cursor: asdadasdadasd123"],
   };
 
